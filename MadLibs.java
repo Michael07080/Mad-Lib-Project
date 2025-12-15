26f3ec588028b9dcee6485913282afe254cfd877
Michael07080 <parisemichael26@gmail.com>
2025-12-15 13:37:39 +0000

    first commit

---
+++ MadLibs.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class MadLibs {
+    
+    
+    
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+
+System.out.print("Enter your first adjective: ");
+    String firstAdj = input.nextLine();
+
+
+System.out.print("Enter your first name: ");
+    String firstName = input.nextLine();
+
+System.out.print("Enter your first place: ");
+    String firstPlace = input.nextLine();
+
+System.out.print("Enter your first noun: ");
+    String firstNoun = input.nextLine();
+
+System.out.print("Enter your second noun: ");
+    String secondNoun = input.nextLine();
+
+System.out.print("Enter your second adjective: ");
+    String secondAdj = input.nextLine();
+
+System.out.print("Enter your first animal: ");
+    String firstAnimal = input.nextLine();
+
+System.out.print("Enter your second name: ");
+    String secondName = input.nextLine();
+
+System.out.print("Enter your first verb: ");
+    String firstVerb = input.nextLine();
+
+System.out.print("Enter your third noun: ");
+    String thirdNoun = input.nextLine();
+
+System.out.print("Enter your fourth noun: ");
+    String fourthNoun = input.nextLine();
+
+System.out.print("Enter your second verb: ");
+    String secondVerb = input.nextLine();
+
+System.out.print("Enter your third verb: ");
+    String thirdVerb = input.nextLine();
+
+System.out.println("Once upon a time a " + firstAdj + " " + firstName + "\nset on an adventure to " + firstVerb + " the famous palace of " + firstPlace + "\nHe took his " + firstNoun + " and his " + secondNoun + " to fight the " + secondAdj + " " + firstAnimal + " named " + secondName + "." + "\nThe beast was known to " + secondVerb + " every form of " + thirdNoun + " in all of " + firstPlace + ".\n" + firstName + " wanted to end this because of his/her's " + fourthNoun + " " + thirdVerb + " towards it.");
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+}
