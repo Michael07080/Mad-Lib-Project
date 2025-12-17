0422c0c3294b61834f990abd988c98ac6949b452
Michael07080 <parisemichael26@gmail.com>
2025-12-17 00:45:19 +0000

    final commit

--- MadLibs.java
+++ MadLibs.java
@@ -11,11 +11,14 @@ System.out.print("Enter your first adjective: ");
     String firstAdj = input.nextLine();
 
 
-System.out.print("Enter your first name: ");
-    String firstName = input.nextLine();
+System.out.print("Enter your first animal: ");
+    String firstAnimal = input.nextLine();
 
-System.out.print("Enter your first place: ");
-    String firstPlace = input.nextLine();
+System.out.print("Enter your first Verb: ");
+    String firstVerb = input.nextLine();
+
+System.out.print("Enter your secondAdj: ");
+    String secondAdj = input.nextLine();
 
 System.out.print("Enter your first noun: ");
     String firstNoun = input.nextLine();
@@ -23,17 +26,14 @@ System.out.print("Enter your first noun: ");
 System.out.print("Enter your second noun: ");
     String secondNoun = input.nextLine();
 
-System.out.print("Enter your second adjective: ");
-    String secondAdj = input.nextLine();
+System.out.print("Enter your third adjective: ");
+    String thirdAdj = input.nextLine();
 
-System.out.print("Enter your first animal: ");
-    String firstAnimal = input.nextLine();
+System.out.print("Enter your first name: ");
+    String firstName = input.nextLine();
 
-System.out.print("Enter your second name: ");
-    String secondName = input.nextLine();
-
-System.out.print("Enter your first verb: ");
-    String firstVerb = input.nextLine();
+System.out.print("Enter your second verb: ");
+    String secondVerb = input.nextLine();
 
 System.out.print("Enter your third noun: ");
     String thirdNoun = input.nextLine();
@@ -41,13 +41,12 @@ System.out.print("Enter your third noun: ");
 System.out.print("Enter your fourth noun: ");
     String fourthNoun = input.nextLine();
 
-System.out.print("Enter your second verb: ");
-    String secondVerb = input.nextLine();
+System.out.print("Enter your first place: ");
+    String firstPlace = input.nextLine();
 
-System.out.print("Enter your third verb: ");
-    String thirdVerb = input.nextLine();
 
-System.out.println("Once upon a time a " + firstAdj + " " + firstName + "\nset on an adventure to " + firstVerb + " the famous palace of " + firstPlace + "\nHe took his " + firstNoun + " and his " + secondNoun + " to fight the " + secondAdj + " " + firstAnimal + " named " + secondName + "." + "\nThe beast was known to " + secondVerb + " every form of " + thirdNoun + " in all of " + firstPlace + ".\n" + firstName + " wanted to end this because of his/her's " + fourthNoun + " " + thirdVerb + " towards it.");
+
+System.out.println("Once upon a time, a " + firstAdj  + " " + firstAnimal + " set off on an adventure to " + firstVerb + " the " + secondAdj + " place." + "\n" +  "He took his " + firstNoun + " and his " + secondNoun + " with him to fight the " + thirdAdj + " animal named " + firstName + "." + "\n" + "The beast was known to " + secondVerb + " everyone, but in the end, the hero used his " + thirdNoun + " and " + fourthNoun + " to defeat it." + "\n" + "Peace returned to the " + firstPlace + ", and the adventure came to and end.");
 
 
 
